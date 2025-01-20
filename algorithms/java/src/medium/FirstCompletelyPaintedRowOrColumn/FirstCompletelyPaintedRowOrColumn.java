c06c17e029ee31dbb773f8088ae64ea1896bcab9
André Silva <advs.94@hotmail.com>
2025-01-20 14:05:39 +0000

    Comments

--- algorithms/java/src/medium/FirstCompletelyPaintedRowOrColumn/FirstCompletelyPaintedRowOrColumn.java
+++ algorithms/java/src/medium/FirstCompletelyPaintedRowOrColumn/FirstCompletelyPaintedRowOrColumn.java
@@ -11,16 +11,16 @@ public class FirstCompletelyPaintedRowOrColumn {
         int[] col = new int[m*n+1];
         for (int r = 0; r < m; r++) {
             for (int c = 0; c < n; c++) {
-                row[mat[r][c]] = r;
-                col[mat[r][c]] = c;
+                row[mat[r][c]] = r; // Track row index for each number
+                col[mat[r][c]] = c; // Track column index for each number
             }
         }
         int[] rowFreq = new int[m];
         int[] colFreq = new int[n];
         for (int i = 0; i < arr.length; i++) {
-            if (++rowFreq[row[arr[i]]] == n) return i;
-            if (++colFreq[col[arr[i]]] == m) return i;
+            if (++rowFreq[row[arr[i]]] == n) return i; // Return index if a row is completely painted
+            if (++colFreq[col[arr[i]]] == m) return i; // Return index if a column is completely painted
         }
-        return arr.length - 1;
+        return arr.length - 1; // Return the last index if no complete row or column is found
     }
 }
