5c4740afaeaf9621ac98bac4fdded2e63bc41446
André Silva <advs.94@hotmail.com>
2025-01-22 22:48:07 +0000

    Update Problem Solution - "1765. Map of Highest Peak"

--- algorithms/java/src/medium/MapOfHighestPeak/MapOfHighestPeak.java
+++ algorithms/java/src/medium/MapOfHighestPeak/MapOfHighestPeak.java
@@ -7,33 +7,43 @@ public class MapOfHighestPeak {
     public int[][] highestPeak(int[][] isWater) {
         int m = isWater.length;
         int n = isWater[0].length;
-        PriorityQueue <int[]> pq = new PriorityQueue<>((a, b) -> isWater[a[0]][a[1]] - isWater[b[0]][b[1]]);
-        boolean[][] visited = new boolean[m][n];
+        int[][] height = new int[m][n];
+        Queue<int[]> queue = new LinkedList<>();
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (isWater[i][j] == 0) {
-                    isWater[i][j] = 1;
+                if (isWater[i][j] == 1) {
+                    height[i][j] = 0;
+                    queue.add(new int[]{i,j});
                 } else {
-                    isWater[i][j] = 0;
-                    visited[i][j] = true;
-                    pq.offer(new int[]{i,j});
+                    height[i][j] = -1;
                 }
             }
         }
-        int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
-        while(!pq.isEmpty()) {
-            int[] coordinates = pq.poll();
-            int x = coordinates[0];
-            int y = coordinates[1];
-            for (int i = 0; i < 4; i++) {
-                int newX = x + directions[i][0];
-                int newY = y + directions[i][1];
-                if (newX >= 0 && newX < m && newY >= 0 && newY < n && !visited[newX][newY]) {
-                    isWater[newX][newY] = isWater[x][y] + 1;
-                    visited[newX][newY] = true;
-                    pq.offer(new int[]{newX,newY});
+        int heightValue = 1;
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                int[] coordinates = queue.poll();
+                int x = coordinates[0];
+                int y = coordinates[1];
+                if (x > 0 && height[x-1][y] == -1) {
+                    height[x-1][y] = heightValue;
+                    queue.add(new int[]{x-1, y});
+                }
+                if (x < height.length-1 && height[x+1][y] == -1) {
+                    height[x+1][y] = heightValue;
+                    queue.add(new int[]{x+1, y});
+                }
+                if (y > 0 && height[x][y-1] == -1) {
+                    height[x][y-1] = heightValue;
+                    queue.add(new int[]{x, y-1});
+                }
+                if (y < height[x].length-1 && height[x][y+1] == -1) {
+                    height[x][y+1] = heightValue;
+                    queue.add(new int[]{x, y+1});
                 }
             }
+            heightValue++;
         }
-        return isWater;
+        return height;
 }
