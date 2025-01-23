43267caf854bace85533e404f0d8ca0e594e74a5
André Silva <advs.94@hotmail.com>
2025-01-23 08:49:33 +0000

    Comments

--- algorithms/java/src/medium/CountServersThatCommunicate/CountServersThatCommunicate.java
+++ algorithms/java/src/medium/CountServersThatCommunicate/CountServersThatCommunicate.java
@@ -9,18 +9,20 @@ public class CountServersThatCommunicate {
         int n = grid[0].length;
         int[] row = new int[m];
         int[] col = new int[n];
+        // Count the number of servers in each row and column
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1) {
-                    row[i]++;
-                    col[j]++;
+                    row[i]++; // Increment row count
+                    col[j]++; // Increment column count
                 }
             }
         }
         int total = 0;
+        // Count servers that can communicate
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1 && (row[i] > 1 || col[j] > 1)) total++;
+                if (grid[i][j] == 1 && (row[i] > 1 || col[j] > 1)) total++; // Add if server communicates
             }
         }
         return total;
