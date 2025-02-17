71592a0c35cdf853dd43f20ed8465dd9828a161b
André Silva <advs.94@hotmail.com>
2025-02-17 14:17:35 +0000

    New Problem Solution - "28. Find the Index of the First Occurrence in a String"

---
+++ algorithms/java/src/easy/FindTheIndexOfTheFirstOccurrenceInAString/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -0,0 +1,10 @@
+package easy;
+
+public class FindTheIndexOfTheFirstOccurrenceInAString {
+    /*
+     * LC - 28. Find the Index of the First Occurrence in a String
+     */
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
