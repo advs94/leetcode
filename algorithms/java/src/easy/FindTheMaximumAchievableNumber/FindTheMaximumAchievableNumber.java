e8877fe362d0e7385a6e1eff2bb88a0fe1ef7775
André Silva <advs.94@hotmail.com>
2025-03-02 11:42:50 +0000

    New Problem Solution - "2769. Find the Maximum Achievable Number"

---
+++ algorithms/java/src/easy/FindTheMaximumAchievableNumber/FindTheMaximumAchievableNumber.java
@@ -0,0 +1,10 @@
+package easy;
+
+public class FindTheMaximumAchievableNumber {
+    /*
+     * LC - 2769. Find the Maximum Achievable Number
+     */
+    public int theMaximumAchievableX(int num, int t) {
+        return num + 2 * t;
+    }
+}
