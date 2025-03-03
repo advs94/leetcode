10c84f3256b053d716f13b69cc52f62ed63ef774
André Silva <advs.94@hotmail.com>
2025-03-03 10:29:57 +0000

    Update Problem Solution - "1980. Find Unique Binary String"

--- algorithms/java/src/medium/FindUniqueBinaryString/FindUniqueBinaryString.java
+++ algorithms/java/src/medium/FindUniqueBinaryString/FindUniqueBinaryString.java
@@ -5,24 +5,9 @@ public class FindUniqueBinaryString {
      * LC - 1980. Find Unique Binary String
      */
     public String findDifferentBinaryString(String[] nums) {
-        StringBuilder sb = new StringBuilder();
-        return dfs(sb, nums);
-    }
-
-    private static String dfs(StringBuilder sb, String[] nums) {
-        if (sb.length() == nums.length) {
-            for (int i = 0; i < nums.length; i++) {
-                if (nums[i].compareTo(sb.toString()) == 0) {
-                    return null;
-                }
-            }
-            return sb.toString();
-        }
-        for (int i = 0; i < 2; i++) {
-            String unique = dfs(sb.append((char)('0'+i)), nums);
-            if (unique != null) return unique;
-            sb.deleteCharAt(sb.length()-1);
-        }
-        return null;
+        final int n = nums.length;
+        char[] result = new char[n];
+        for (int i = 0; i < n; i++) result[i] = (char) (nums[i].charAt(i) ^ 1);
+        return new String(result);
     }
 }
