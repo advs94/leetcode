a0f0da9cadc88248820fd78d93ef6861b0137a3c
André Silva <advs.94@hotmail.com>
2025-04-03 23:36:11 +0100

    Update Problem Solution - "3432. Count Partitions with Even Sum Difference"

--- algorithms/java/src/easy/CountPartitionsWithEvenSumDifference/CountPartitionsWithEvenSumDifference.java
+++ algorithms/java/src/easy/CountPartitionsWithEvenSumDifference/CountPartitionsWithEvenSumDifference.java
@@ -1,7 +1,16 @@
 package easy;
 
 public class CountPartitionsWithEvenSumDifference {
-    public void countPartitionsWithEvenSumDifference() {
-        // TODO: Implement solution
+    public int countPartitions(int[] nums) {
+        int sumLeft = 0, sumRight = 0, count = 0;
+        for (int num : nums) sumRight += num;
+        for (int i = 0; i < nums.length - 1; i++) {
+            sumRight -= nums[i];
+            sumLeft += nums[i];
+            if (((sumLeft - sumRight) & 1) == 0) {
+                count++;
+            }
+        }
+        return count;
     }
 }
